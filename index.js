eb61d00e66ac890ce52d3964a5364d8f6a5937cb
Aden Addis <adenwonde56@gmail.com>
2022-06-27 11:48:11 -0400

    commit tree 4bfce18a35bf52ff270c12e09e55839ce8716509

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// let companyName = "Scuber"
+// make it const
+const companyName = "Scuber"
+
+let mostProfitableNeighborhood = "Chelsea"
+
+let companyCeo = "Susan Smith"
+
